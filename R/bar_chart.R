ef2d55c0b7cc049a4436c7402790985a090d570f
Oscar Gonzalez Velasco <oscargvelasco@gmail.com>
2024-01-22 17:07:51 +0100

    Stacked barplot with group annotation 1st attempt

--- R/bar_chart.R
+++ R/bar_chart.R
@@ -22,11 +22,13 @@ bar_chart <- function(scObject=NULL, main_variable=NULL, subtype_variable=NULL, 
   names(colorOrder) <- order
   if(!is.null(sample_id)){
     samples <- as.character(main_metadata[, sample_id])
+    n.of.stack.bars <- table(unique(data.frame(groups, samples))$groups)
     df <- data.frame(groups,covariable,samples)
     df <- data.frame(groups=paste(groups,samples,sep = "_"), covariable)
     contig_tab <- apply(table(df), 1, function(row){row/sum(row)})
   }else{
     df <- data.frame(groups, covariable)
+    n.of.stack.bars <- table(unique(data.frame(groups))$groups)
     contig_tab <- apply(table(df),1,function(row){row/sum(row)})
   }
   group_names <- colnames(contig_tab)
@@ -40,12 +42,24 @@ bar_chart <- function(scObject=NULL, main_variable=NULL, subtype_variable=NULL, 
     #colorOrder <- c(colorOrder[names(colorOrder)!=subtype_only],colorOrder[names(colorOrder)==subtype_only])
     #order <- c(order[!(order %in% subtype_only)], subtype_only)
   }
+  xmin.annotation <- c(0.5, cumsum(n.of.stack.bars)[1:length(n.of.stack.bars)-1]+0.5)
+  xmax.annotation <- c(cumsum(n.of.stack.bars)[2:length(n.of.stack.bars)], Inf)+0.5
   g <- ggplot2::ggplot(contig_tab_resh, ggplot2::aes(x=groups, y=value, group_by=covariable, fill = covariable)) +
     ggplot2::geom_bar(position="stack", stat="identity") +
     ggplot2::theme_minimal() +
     ggplot2::theme(axis.text.x=ggplot2::element_text(angle=45, vjust=1, hjust=1,size = 12)) +
     ggplot2::scale_fill_manual(values = colores[colorOrder], drop=FALSE) +
     ggplot2::ggtitle(paste("Proportions of", subtype_variable, "by", main_variable)) +
-    ggplot2::scale_y_continuous(name="percentage", breaks =  seq(0, 1, by=0.1), labels = seq(0, 100, by=10))
+    ggplot2::scale_y_continuous(name="percentage", breaks =  seq(0, 1, by=0.1), labels = seq(0, 100, by=10)) +
+    # ggplot2::geom_rect(ggplot2::aes(xmin = -sd.montecarlo, xmax = sd.montecarlo, ymin = as.integer(classLabel) - 0.5, ymax = as.integer(classLabel) + 0.5),
+    #                    fill = "pink", alpha = 0.3) +
+    ggplot2::annotate(
+      ymin = -0.02, ymax = -0.1,
+      xmin = xmin.annotation,
+      xmax = xmax.annotation,
+      geom = "rect",
+      fill = rev(colores)[1:length(n.of.stack.bars)]
+      #fill = c("dodgerblue", "tan")
+    )
   return(g)
 }
