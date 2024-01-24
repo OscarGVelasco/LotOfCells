ea7815b7c27c3ea6c4c60c79e1d029ab47de7e22
Oscar Gonzalez Velasco <oscargvelasco@gmail.com>
2024-01-24 11:05:32 +0100

    Plot abundance test colors uptaded with out of bound color

--- R/plotAbundanceTest.R
+++ R/plotAbundanceTest.R
@@ -30,9 +30,9 @@ plotAbundanceTest <- function(tableResults=NULL, subtype_variable){
   print(df$significance)
   ggplot2::ggplot(df, ggplot2::aes(x=groupFC, y=classLabel)) +
     ggplot2::geom_point(ggplot2::aes(fill=significance), pch=21, stroke=0.2, size=8, alpha=0.8) +
-    ggplot2::scale_fill_gradientn(colours=c("#142F5D","#142F5D", "#8BBCD4", "#C1DEEF","#EEF6FF", "#FDFFFF", "#F6F3FF", "#DDCFFF", "#D1AADB", "#76608E","#76608E"),
+    ggplot2::scale_fill_gradientn(colours=c("#122A53","#43587D", "#8BBCD4", "#C1DEEF","#EEF6FF", "#FDFFFF", "#F6F3FF", "#DDCFFF", "#D1AADB", "#76608E","#463955"),
                                   limits = c(-3, 3),
-                                  labels=c("p.val 0.001", 0.01,0.1,1,0.1,0.01,"p.val 0.001")) +
+                                  labels=c("p.val 0.001", 0.01,0.1,1,0.1,0.01,"p.val 0.001"), oob=scales::squish) +
     ggplot2::scale_color_manual(values = "#eeefef") +
     ggplot2::geom_errorbarh(ggplot2::aes(xmin=CI95low, xmax=CI95high),
                   position=ggplot2::position_dodge(.9),height = 0.1, linewidth = 0.3, colour="#70508E") +
